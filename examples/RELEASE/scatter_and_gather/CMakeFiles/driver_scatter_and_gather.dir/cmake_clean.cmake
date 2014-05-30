13358e71ccb115a9adf047e359839723e1fdb815
Ang Li <ali28@wisc.edu>
2014-05-30 01:27:21 -0500

    Fix the example of scattering

---
+++ examples/RELEASE/scatter_and_gather/CMakeFiles/driver_scatter_and_gather.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/driver_scatter_and_gather.dir/./driver_scatter_and_gather_generated_driver_scatter_and_gather.cu.o"
+  "driver_scatter_and_gather.pdb"
+  "driver_scatter_and_gather"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/driver_scatter_and_gather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
